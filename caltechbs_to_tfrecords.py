e996f1d489778266861d1775ac81938fb2e76933
Yash <ysharma1126@gmail.com>
2017-05-03 10:34:28 -0400

    fix truncated and difficult

--- caltechbs_to_tfrecords.py
+++ caltechbs_to_tfrecords.py
@@ -89,12 +89,12 @@ def write_images_from_directory(set_directory_name, set_directory_path, annotati
                     else:
                         truncated_f.append(0)
                     difficult_f.append(0)
-                elif object_dict['lbl'] == 'person?':
-                    truncated_f.append(0)
-                    difficult_f.append(1)
-                else:
-                    truncated_f.append(0)
-                    difficult_f.append(0)
+                #elif object_dict['lbl'] == 'person?':
+                #    truncated_f.append(0)
+                #    difficult_f.append(1)
+                #else:
+                #    truncated_f.append(0)
+                #    difficult_f.append(0)
                 # Can check whether the object is occluded or not by 
                 # accessing object_dict['ocl'] == 1, if its 1, then it
                 # is occluded.  The associated bbox for the predicted
